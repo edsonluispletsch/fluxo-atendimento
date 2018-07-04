ed45903250b46cf1fe35af5c60ea6893f77441b8
Edson Luis Pletsch <Edson.pletsch@linx.com.br>
2018-07-04 16:55:08 -0300

    teste

--- src/app/cliente/cadastro-cliente/cadastro-cliente.component.ts
+++ src/app/cliente/cadastro-cliente/cadastro-cliente.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroClienteComponent implements OnInit {
 
+  cliente: string = "Proprietário";
+  contador: number = 0;
+  imagem: string;
+  
   constructor() { }
 
   ngOnInit() {
+    let self = this;
+
+    setTimeout(() => {
+      self.cliente = "Proprietário iniciando...";
+      self.imagem = "Imagem/NeymarEmPe.jpg";
+    }, 1000);
+
+    let interval = setInterval(() => {
+      self.cliente = "Proprietário " + self.contador++;
+      if (self.contador == 5){
+        clearInterval(interval);
+        self.imagem = "Imagem/NeymarCaido.jpg";
+      }
+    }, 2000);
+  }    
+
+  LevantarNeymar() : void {
+    this.imagem = 'Imagem/NeymarEmPe.jpg';
   }
 
+  DerrubarNeymar() : void {
+    this.imagem = 'Imagem/NeymarCaido.jpg';
+  }  
 }
--- src/app/cliente/tipo-cliente/tipo-cliente.component.ts
+++ src/app/cliente/tipo-cliente/tipo-cliente.component.ts
@@ -9,7 +9,6 @@ export class TipoClienteComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
